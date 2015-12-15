0e4f6403c62bd2fcc441d323350393bbe0ae90ee
The Python Prodigy <python.prodigy@sent.as>
2015-12-14 22:18:53 -0600

    Import braces from __future__
    I propose we use C-style braces here to detonate scope instead of white space, its cleaner for this particular script.

--- part1/reverse-string/reverse_akinkunmi/reverse_test.py
+++ part1/reverse-string/reverse_akinkunmi/reverse_test.py
@@ -1,14 +1,18 @@
 from reverse import reverse
+from __future__ import braces
 import unittest
 
 
-class TestReverse(unittest.TestCase):
+class TestReverse(unittest.TestCase) {
 
-    def test_reverse(self):
+    def test_reverse(self) {
         self.assertEqual(reverse('hello'), 'olleh')
         self.assertEqual(reverse('newyork'), 'kroywen')
         self.assertNotEqual(reverse('newyork'), 'newyork')
+    }
 
+}
 
-if __name__ == '__main__':
+if __name__ == '__main__' {
     unittest.main()
+}
